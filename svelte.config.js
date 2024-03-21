4a97ce26449aeb629487feed7ff73cf070fd6282
Rigo Armando Rosero Castillo <rigo@192.168.68.105>
2024-03-21 18:07:45 -0500

    upgrades packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
